6a9d4dfd3b5611d0a2284929c5fae70bbc5bdddc
kloudkitsune <corbansmith92@gmail.com>
2025-11-26 19:43:00 -0700

    Feat: Added Footer.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,6 +7,7 @@ import ModalWithForm from "../Main/ModalWithForm/ModalWithForm";
 import ItemModal from "../ItemModal/ItemModal";
 import { getWeather, filterWeatherData } from "../../utils/weatherApi";
 import { coordinates, APIkey } from "../../utils/constants";
+import Footer from "../Footer/Footer";
 
 function App() {
   const [weatherData, setWeatherData] = useState({
@@ -97,6 +98,7 @@ function App() {
         card={selectedCard}
         onClose={closeActiveModal}
       />
+      <Footer />;
     </div>
   );
 }
